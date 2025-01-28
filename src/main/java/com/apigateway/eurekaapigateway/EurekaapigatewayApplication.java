78bc8b429b83500319faa39db6360d75ba3da363
krskabhi98 <kr.skabhi@gmail.com>
2025-01-28 18:14:48 +0530

    initial commit

---
+++ src/main/java/com/apigateway/eurekaapigateway/EurekaapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.apigateway.eurekaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaapigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apigateway/eurekaapigateway/EurekaapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.eurekaapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
